c61b4547995349f54e0330324ce53c971c6fb659
Vladislav Bogdanov <vla-bogdano@ya.ru>
2023-01-17 14:06:09 +0300

    Add some code

---
+++ Drivers/Mongoose/Inc/mongoose_custom.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//#define MG_ARCH MG_ARCH_NEWLIB
+//#define MG_ENABLE_MIP 1
+//#define MG_ENABLE_CUSTOM_MILLIS 1
+//#define MG_ENABLE_FILE 0
+//#define MG_ENABLE_PACKED_FS 1
+
+//#define MIP_DEBUG 1
+//#define MG_ENABLE_LINES 1
+
+
